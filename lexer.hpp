cef15bdcbf771d22b39d6c6d9123a879cbfa0232
Aditya Diwakar <aditya@diwakar.io>
2023-11-03 14:40:05 -0400

    add fsm based string parsing

--- lexer.hpp
+++ lexer.hpp
@@ -5,10 +5,21 @@
 #include <string_view>
 #include <iostream>
 
+#include "tokens.hpp"
+
 namespace lexer {
   enum class States {
     START,
+    CONSUME_CHAR,
     DONE,
+    CHECK_STR,
+    CHECK_NEXT, // TODO
+    BUFFER_ADD,
+    STR_RISING_EDGE,
+    STR_FALLING_EDGE,
+
+    START_TOKEN_FROM_BUFFER,
+    FINISH_TOKEN_FROM_BUFFER,
   };
 
 	std::string stateToString(States state);
@@ -16,7 +27,15 @@ namespace lexer {
   class Engine {
     private:
       States curr_state_;
+      types::Tokens production_type_;
       size_t char_idx_ = 0;
+      size_t buffer_idx_ = 0;
+      char c_;
+
+      /* nice default inits that should be done explicitly in verilog */
+      bool ident_flag_  = false;
+      bool num_flag_    = false;
+      bool str_flag_    = false;
 
       char char_stream_[1000];
       char buffer_[100];
